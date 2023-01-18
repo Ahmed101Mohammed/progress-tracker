f648e5520ec397b737f50f9dda58e9190ceb7d24
Ahmed Mohamed al3adl <ahmedmohamedal3adl@gmail.com>
2023-01-18 14:19:45 +0200

    This file contain the structure of the dataBas for the App

---
+++ data_base_structure.py
@@ -0,0 +1,32 @@
+data_structure = [
+                    {
+                        "table_title": "users",
+                        "table_columons": [ "id INTEGER PRIMARY KEY AUTOINCREMENT",
+                                            "name TEXT UNIQUE",
+                                            "hours REAL"
+                                            ]
+                    }
+                    ,{
+                        "table_title": "subjects",
+                        "table_columons": [ "id INTEGER PRIMARY KEY AUTOINCREMENT",
+                                            "title TEXT UNIQUE"
+                                            ]
+                    }
+                    ,{
+                        "table_title": "goals",
+                        "table_columons": [ "id INTEGER PRIMARY KEY AUTOINCREMENT",
+                                            "user_id INTEGER",
+                                            "subject_id INTEGER",
+                                            "target_houres REAL",
+                                            "progress REAL"
+                                            ]
+                    }
+                    ,{
+                        "table_title": "commits",
+                        "table_columons": [ "id INTEGER PRIMARY KEY AUTOINCREMENT",
+                                            "goal_id INTEGER",
+                                            "hours REAL",
+                                            "comment TEXT",
+                                            ]
+                    }
+                ]
